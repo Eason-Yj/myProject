bb486bc99ad475630b3f251c45e247f4eec04389
wangyuji <wangyuji@qq.com>
2022-11-09 17:31:53 +0800

    prime_number 1

---
+++ algorithm/simple_algorithm/prime_number.py
@@ -0,0 +1,67 @@
+import time
+
+
+class Prime():
+
+    def __init__(self, start_num=1, end_num=2):  # type:(int, int)->None
+        self.__start_num = start_num
+        self.__end_num = end_num
+        self.prime_list = []
+        self.composite_list = []
+        self.factor_list = []
+
+    def isPrime(self, num):
+        """
+        判断某数是否为质数,不是则返回所有的因子
+        :param num:
+        :return:
+        """
+        factor_list = []
+        min_factor = 2
+        max_factor = num // 2
+
+        for i in range(min_factor, max_factor + 1):
+            if num % i == 0:
+                factor_list.append(i)
+        if len(factor_list) != 0:
+            self.factor_list = factor_list
+            return False
+        else:
+            return True
+
+    def get_prime_list(self):
+        prime_list = []
+        if self.composite_list:
+            prime_list = list(range(self.__start_num, self.__end_num + 1))
+            for num in self.composite_list:
+                prime_list.remove(num)
+        else:
+            for num in range(self.__start_num, self.__end_num + 1):
+                if self.isPrime(num):
+                    prime_list.append(num)
+
+        self.prime_list = prime_list
+        return prime_list
+
+    def get_composite_list(self):
+        composite_list = []
+        if self.prime_list:
+            composite_list = list(range(self.__start_num, self.__end_num + 1))
+            for num in self.prime_list:
+                composite_list.remove(num)
+        else:
+            for num in range(self.__start_num, self.__end_num + 1):
+                print(num, self.isPrime(num))
+                if not self.isPrime(num):
+                    composite_list.append(num)
+
+        self.composite_list = composite_list
+        return composite_list
+
+
+if __name__ == '__main__':
+    time1 = time.time()
+    prime = Prime(end_num=10000)
+    prime.get_prime_list()
+    print(prime.prime_list)
+    print(time.time() - time1)
